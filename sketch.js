a1956772cdeed0310720351c2790143734f3c531
joao098765 <00001114087981sp@al.educacao.sp.gov.br>
2024-08-19 08:20:38 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,9 @@
+let winWidth = 400;
+let winHeight = 300;
+function setup() {
+  // Create the canvas (adjust width and height as needed)
+  let canvas = createCanvas(winWidth, winHeight);
+}
+function draw() {
+  background(51);
+}
